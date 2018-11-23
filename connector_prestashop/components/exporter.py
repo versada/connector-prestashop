12b6151dab7839184c67ac815e850a2fe175add8
Marc Poch @PlanetaTIC <mpoch@planetatic.com>
2018-11-23 11:31:20 +0100

    [FIX]connector_prestashop: Fix to be able to export product attributes on product.product exportation.

--- connector_prestashop/components/exporter.py
+++ connector_prestashop/components/exporter.py
@@ -127,7 +127,7 @@ class PrestashopExporter(AbstractComponent):
         binding = None
         # wrap is typically True if the relation is a 'product.product'
         # record but the binding model is 'prestashop.product.product'
-        wrap = relation._model._name != binding_model
+        wrap = relation._name != binding_model
         if wrap and hasattr(relation, binding_field_name):
             domain = [(self._openerp_field, '=', relation.id),
                       ('backend_id', '=', self.backend_record.id)]
@@ -158,7 +158,7 @@ class PrestashopExporter(AbstractComponent):
                     # do never commit during tests
                     if not getattr(threading.currentThread(), 'testing',
                                    False):
-                        self._cr.commit()  # pylint: disable=invalid-commit
+                        self.env.cr.commit()  # pylint: disable=invalid-commit
         else:
             # If prestashop_bind_ids does not exist we are typically in a
             # "direct" binding (the binding record is the same record).
